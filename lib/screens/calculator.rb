0bb6a7b6e1b4dabc53bdb30455d44831d3b855ef
Levi Wilson <levi@leviwilson.com>
2013-09-04 23:22:16 -0400

    static text fields are labels

--- lib/screens/calculator.rb
+++ lib/screens/calculator.rb
@@ -4,13 +4,9 @@ class Calculator
 
   button(:add, id: '93')
   button(:calculate, value: 'Equals')
-  control(:result, id: '150')
+  label(:result, id: '150')
 
   def press(number)
     adapter.button(id: "13#{number}").click
-  end
-
-  def result
-    result_view.control_name
   end
 end
